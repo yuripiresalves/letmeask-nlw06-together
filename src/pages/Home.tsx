7698b459a984c129e091b76b1bc387fd02ef63dd
Yuri Alves <yuripiresalves@gmail.com>
2021-06-25 00:18:25 -0300

    aula 04

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -34,7 +34,12 @@ export function Home() {
     const roomRef = await database.ref(`rooms/${roomCode}`).get();
 
     if (!roomRef.exists()) {
-      alert('Room does not exist');
+      alert('Room does not exist.');
+      return;
+    }
+
+    if (roomRef.val().closedAt) {
+      alert('Room is already closed.');
       return;
     }
 
